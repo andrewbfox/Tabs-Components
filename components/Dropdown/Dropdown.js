d3c1e2f12b09e503402efab207f7551497cdfd0b
Andrew Fox <andrewbfox@gmail.com>
2019-06-20 16:37:13 -0400

    Modified dropdown.js to remove 'console.log's

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -3,13 +3,13 @@ class Dropdown {
     
     // Assign this.element to the dropdown element
     this.element = element;
-    console.log('this.element: ', this.element);
+    
     // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
     this.button = this.element.querySelector('.dropdown-button');
-    console.log('this.button: ', this.button);
+    
     // assign the reference to the ".dropdown-content" class found in the dropdown element
     this.content = this.element.querySelector('.dropdown-content');
-    console.log('this.content: ', this.content);
+    
     // Add a click handler to the button reference and call the toggleContent method.
     this.button.addEventListener('click', () => {
       this.toggleContent();
@@ -26,4 +26,3 @@ class Dropdown {
 
 // Nothing to do here, just study what the code is doing and move on to the Dropdown class
 let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
-console.log('dropdowns: ', dropdowns);
